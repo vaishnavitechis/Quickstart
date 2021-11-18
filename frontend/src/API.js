86b3f4f4f4ad78670764cd431f02466ff2065c72
vaishnavi <akash@gmail.com>
2021-11-18 09:20:57 +0530

    commit

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -7,7 +7,7 @@ var baseURL;
 // } else {
 //     baseURL = 'http://127.0.0.1:8000';
 // }
-baseURL = 'http://127.0.0.1:8000';
+baseURL = 'https://backend1999.herokuapp.com/';
 const api = axios.create({
     baseURL: baseURL,
     headers: {
